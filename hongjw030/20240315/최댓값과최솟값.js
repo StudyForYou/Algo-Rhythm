9730e761174a52011550c93deeef80a6b4332a1b
HellenHong <hongjw030@naver.com>
2024-03-13 16:34:09 +0900

    PGS: 최댓값과 최솟값

---
+++ hongjw030/20240315/최댓값과최솟값.js
@@ -0,0 +1,9 @@
+// 프로그래머스 2레벨, 최댓값과 최솟값.
+
+function solution(s) {
+  const array = s.split(" ");
+  array.sort((a, b) => a-b );
+  return `${array[0]} ${array[array.length -1]}`;
+}
+
+console.log(solution("-1 -2 -3 -4"))
