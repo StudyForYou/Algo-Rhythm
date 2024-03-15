b0b063e2e72ab0ff956872c2404d0b89223e1a44
SJvaca30 <yyvaca30@gmail.com>
2024-03-15 09:53:33 +0900

    PGS: 직사각형 별찍기

---
+++ SJvaca30/20240315/직사각형 별찍기.js
@@ -0,0 +1,13 @@
+/**
+https://school.programmers.co.kr/learn/courses/30/lessons/12969
+ */
+
+let result = "";
+for (let i = 0; i < a; i++) {
+  for (let j = 0; j < b; j++) {
+    result += "*";
+  }
+  result += "\n";
+}
+
+console.log(result);
