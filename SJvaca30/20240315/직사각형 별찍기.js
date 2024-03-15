85c26a96145b59f315755aa524024dbbe441fbd0
SJvaca30 <yyvaca30@gmail.com>
2024-03-15 09:59:55 +0900

    PGS: 직사각형 별찍기(풀이추가)

--- SJvaca30/20240315/직사각형 별찍기.js
+++ SJvaca30/20240315/직사각형 별찍기.js
@@ -2,12 +2,35 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/12969
  */
 
-let result = "";
-for (let i = 0; i < a; i++) {
-  for (let j = 0; j < b; j++) {
-    result += "*";
-  }
-  result += "\n";
-}
+// 첫번째
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const b = Number(n[0]),
+    a = Number(n[1]);
 
-console.log(result);
+  let result = "";
+  for (let i = 0; i < a; i++) {
+    for (let j = 0; j < b; j++) {
+      result += "*";
+    }
+    result += "\n";
+  }
+
+  console.log(result);
+});
+
+// 두번째
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const b = Number(n[0]),
+    a = Number(n[1]);
+
+  const result = Array(a)
+    .fill("*")
+    .map(() => Array(b).fill("*").join(""))
+    .join("\n");
+
+  console.log(result);
+});
