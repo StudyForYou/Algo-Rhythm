4bb6d480d9c374a995ac7d25e74cca5524686bb2
남궁수영 <sp38013@naver.com>
2024-03-15 15:41:46 +0900

    PSG: 삼각형의 완성조건 (2)

---
+++ ngsy/20240318/삼각형의 완성조건 (2).js
@@ -0,0 +1,23 @@
+/**직사각형 별찍기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=javascript
+ * a: 가장 긴 변
+ * a < ( b + c)
+ */
+
+function solution(sides) {
+  const maxLine = Math.max(...sides);
+  const minLine = Math.min(...sides);
+  const sumLine = maxLine + minLine;
+  let count = 0;
+
+  // sides에 가장 긴 변이 있는 경우
+  for (let i = 1; i <= maxLine; i++) {
+    let sum = i + minLine;
+    if (sum > maxLine) count += 1;
+  }
+
+  // sides에 가장 긴 변이 없는 경우
+  for (let j = maxLine + 1; j < sumLine; j++) count += 1;
+
+  return count;
+}
