e86af33f7208407231dce20463ce87bbed948ffb
전수빈 <soobin08011812@gmail.com>
2024-04-03 09:15:37 +0900

    PGS: delete 최소직사각형

--- SoobinJ/20240403/최소직사각형.js
+++
@@ -1,8 +0,0 @@
-function solution(sizes) {
-  let c = [...sizes].reduce((acc, el) => {
-    acc.push(Math.min(el[0], el[1]));
-    return acc;
-  }, []);
-
-  return Math.max(...sizes.flat()) * Math.max(...c);
-}
