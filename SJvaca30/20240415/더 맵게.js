0dcd1cc0dbccb5710d2ffb9b6336ab7ed448de32
SJvaca30 <yyvaca30@gmail.com>
2024-04-15 09:05:45 +0900

    fix

--- SJvaca30/20240415/더 맵게.js
+++ SJvaca30/20240415/더 맵게.js
@@ -1,8 +1,3 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42626
 
-function solution(scoville, K) {
-  const heap = new MinHeap(scoville);
-    let count = 0;
-
-    while
-}
+function solution(scoville, K) {}
